1dfd3565ac5b0be8c023964cd2742ea3c55993fa
bytezer <himura.js@hotmail.com>
2025-06-11 15:38:38 -0500

    refractor: Default styles removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { MyOrders } from "./pages/MyOrders";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="my-orders" element={<p>My orders</p>} />
+        <Route path="my-orders" element={<MyOrders />} />
         <Route path="add-order" element={<p>Create a new order</p>} />
         <Route path="edit-order" element={<p>Edit order</p>} />
       </Routes>
